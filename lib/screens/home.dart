ce846bcf7c8ef16bf14d51a8cbe6a71d13a7a198
Thobif <jirayootpandee537@gmail.com>
2022-01-06 15:01:09 +0700

    'update'

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'scaffold_demo..dart';
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}):super(key: key);
+import 'appbar_demo.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Flutter App Development',
       debugShowCheckedModeBanner: false,
-      home: ScaffoldDemo(),
-      theme: ThemeData(primarySwatch: Colors.red),
+      home: const AppBarDemo(),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
-
